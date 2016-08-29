55d0bdf2177d565a33dd6688b994affe6c71afc5
Anderson Berrocal <Anderson.berrocal@andeansystems.com>
2016-08-29 17:04:46 -0500

    Cambios ListaUsuario

---
+++ Gdoc.Entity/Extension/EUsuario.cs
@@ -0,0 +1,19 @@
+﻿using Gdoc.Entity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Gdoc.Entity.Extension
+{
+    [NotMapped]
+    public class EUsuario:Usuario
+    {
+        public Concepto Cargo { get; set; }
+        public Concepto TipoUsuario { get; set; }
+        public Concepto Area { get; set; }
+        public Concepto ClaseUsu { get; set; }
+    }
+}
