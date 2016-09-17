8be21b74e7ce084206cf5e491913a16a33260b50
other <ajjskdjkjxk>
2016-09-17 14:47:26 -0500

    bussines 02 47

---
+++ Gdoc.Negocio/NModuloPaginaUrl.cs
@@ -0,0 +1,33 @@
+﻿using Gdoc.Dao;
+using Gdoc.Entity.Extension;
+using Gdoc.Entity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gdoc.Negocio
+{
+    public class NModuloPaginaUrl:IDisposable
+    {
+        #region "Variable"
+        protected DModuloPaginaUrl dModuloPaginaUrl = new DModuloPaginaUrl();
+        #endregion
+        public List<EModuloPaginaUrl> ObtenerPaginaModuloUrl(Usuario eUsuario) {
+            try
+            {
+                return dModuloPaginaUrl.ObtenerPaginaModuloUrl(eUsuario);
+            }
+            catch (Exception ex)
+            {
+                
+                throw ex;
+            }
+        }
+        public void Dispose()
+        {
+            dModuloPaginaUrl = null;
+        }
+    }
+}
